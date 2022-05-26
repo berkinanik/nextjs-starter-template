db288b59f7949ceaf1405fa7d8d4b73a5769ae0d
Berkin Anik <Berkin.Anik@d-teknoloji.com.tr>
2022-05-26 14:00:18 +0300

    updates on configuration files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
+  cleanDistDir: true,
+  distDir: '.next',
+  eslint: {
+    dirs: ['src'],
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'next/core-web-vitals',
+    'prettier'
+  ],
   reactStrictMode: true,
 };
